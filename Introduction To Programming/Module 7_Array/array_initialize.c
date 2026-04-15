7394e71c5fa0239ba74889e3d60e113b02347fe3
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-04-15 12:15:11 +0600

    array initiallze

---
+++ Introduction To Programming/Module 7_Array/array_initialize.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main () {
+    int student[5] ={5 ,5,32,6,6} ;
+    // printf("%d", student[4]);
+    for(int i = 0; i <5 ; i++)
+    {
+        printf("%d ", student[i]);
+    }
+    return 0;
+}
