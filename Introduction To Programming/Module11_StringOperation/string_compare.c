5ab6fe82405cd8a1208a74ac117245c4f5cbd560
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-04-23 08:27:29 +0600

    string compare

---
+++ Introduction To Programming/Module11_StringOperation/string_compare.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+    char a[101], b[101];
+    scanf("%s %s", a, b);
+    int val = strcmp(a,b) ;
+    if (val < 0 ){
+        printf("A is smaller");
+    }else if(val> 0){
+        printf("B is smaller");
+    }else if (val == 0) {
+        printf("same");
+    }
+    return 0;
+}
