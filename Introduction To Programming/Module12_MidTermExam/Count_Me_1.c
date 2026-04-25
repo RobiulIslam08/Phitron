4550b4e8dd412d6f2ecd5635191277c219d2d341
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-04-25 15:21:17 +0600

    count me 1 c problem solve

---
+++ Introduction To Programming/Module12_MidTermExam/Count_Me_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main () {
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i =0; i<n; i++) {
+        scanf("%d", &a[i]);
+    }
+    int countTwo = 0;
+    int countThree = 0;
+    for(int i = 0; i<n; i++){
+        if(a[i] % 2 == 0){
+            countTwo++;
+        }else  if(a[i] % 3 == 0){
+            countThree++;
+        }
+    }
+
+   
+
+    printf("%d %d", countTwo, countThree);
+    return 0;
+}
