01537351c7cb0cb6f0243b1de8b88ad491c4a1a0
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-05-20 17:37:16 +0600

    learn input in c++

---
+++ Introduction to C++ for DSA/input.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x, y;
+    double z;
+
+    cin >> x >> y >> z;
+    cout << x <<endl << y << endl << z << endl;
+    char a = 'b';
+    // cout << (int)a;
+    int ascii = a;
+    cout << ascii << endl;
+    int p = 65;
+    cout << (char) p;
+
+
+    return 0;
+}
---
+++ Introduction to C++ for DSA/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main() {
+   int x = 100;
+   char c = 'a'; 
+   double d = 400.3;
+
+    std :: cout << x << " " << c << " " << std::endl << d << std:: endl;
+    std :: cout << "My favorit number is  " << x ;
+    return 0;
+}
