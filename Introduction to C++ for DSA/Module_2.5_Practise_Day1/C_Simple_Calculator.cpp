979fe375230c90c0ede6afbc3ae07a534741b423
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-06-02 11:29:55 +0600

    C. Simple Calculator

---
+++ Introduction to C++ for DSA/Module_2.5_Practise_Day1/C_Simple_Calculator.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int m,n;
+    cin >> n >> m;
+    cout << n << ' ' << '+'  << ' '  << m  << ' ' << '='  << ' ' << n + m << endl;
+    cout << n << ' ' << '*'  << ' '  << m  << ' ' << '='  << ' ' << n * m << endl;
+    cout << n << ' ' << '-'  << ' '  << m  << ' ' << '='  << ' ' << n - m << endl;
+    return 0;
+}
