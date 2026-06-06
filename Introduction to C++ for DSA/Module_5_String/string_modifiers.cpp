66c0059126ea253f229043e68a2f968215f1c389
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-06-06 12:19:41 +0600

    iterators

---
+++ Introduction to C++ for DSA/Module_5_String/string_modifiers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "hello robi";
+    // string s2 = "hello";
+    // s += s2;
+    // s2.append(s);
+    // s.push_back('m');
+    // s.replace(6,0, "mr ");
+    s.insert(6, "Robi ");
+   
+    cout << s << endl;
+    return 0;
+}
