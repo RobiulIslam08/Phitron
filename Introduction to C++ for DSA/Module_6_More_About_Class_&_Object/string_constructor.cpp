928c686dd20a530f6c8d6af41483aa4bf95b6d23
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-06-08 11:59:19 +0600

    string constructor

---
+++ Introduction to C++ for DSA/Module_6_More_About_Class_&_Object/string_constructor.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string  s = "hello";
+    // 1
+    // string s("hello0");
+    // 2
+    //string s("hello0",3);
+    //3
+    // string s = "hello world";
+    // string t(s, 4);
+    //4
+    string s(444444, 'A');
+
+    cout << s << endl;
+    return 0;
+}
