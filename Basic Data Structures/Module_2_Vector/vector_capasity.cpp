0b17735384d757afd732dc17d6e4e1432b698246
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-06-24 15:16:10 +0600

    vector modifires

---
+++ Basic Data Structures/Module_2_Vector/vector_capasity.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    cout << v.capacity();
+    v.push_back(10);
+        cout << v.capacity();
+    v.push_back(10);
+        cout << v.capacity();
+    return 0;
+}
