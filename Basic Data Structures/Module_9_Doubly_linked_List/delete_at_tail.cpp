430e7d451783881bd1af6bb6e82c328acd472a1f
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-07-17 12:09:40 +0600

    delte at tail

---
+++ Basic Data Structures/Module_9_Doubly_linked_List/delete_at_tail.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward (Node *head) {
+    Node *temp = head;
+    
+    while (temp != NULL)
+    {
+        /* code */
+        cout << temp->val << ' ' ;
+        temp = temp->next;
+    }
+    cout << endl;
+
+    
+}
+
+void delete_at_tail (Node *&head, Node* &tail){
+   Node *deletenode = tail;
+   tail = tail->prev;
+   delete deletenode;
+   if(tail == NULL) 
+   {
+        head = NULL ;
+        return;
+   }
+   tail->next = NULL;
+   
+
+}
+int main()
+{
+    Node *head = new Node(20);
+    Node *a = new Node(30);
+    Node *tail = new Node(40);
+
+    head->next = a;
+    a->prev =head;
+
+    a->next = tail;
+    tail->prev = a;
+
+    delete_at_tail(head, tail);
+    
+  
+
+    print_forward(head);
+    return 0;
+}
