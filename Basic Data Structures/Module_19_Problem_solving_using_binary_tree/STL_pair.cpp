5abd61272feab38c599851ee5a518a927a47b1b9
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-08-01 08:46:01 +0600

    stl pair

---
+++ Basic Data Structures/Module_19_Problem_solving_using_binary_tree/STL_pair.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    pair<int, string> p;
+    // p = make_pair(3, "hello");
+    // sortcut
+    p= {5,"Hi"};
+    cout << p.first << p.second;
+    return 0;
+}
