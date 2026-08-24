5beb3223dc66938abdeea82812c6982a306c498a
Rabiul Islam <robiulislamrobi0874@gmail.com>
2026-08-24 10:47:02 +0600

    biscuit generator

---
+++ XPSC/Week_One/Biscuit_Generator.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, t;
+    cin >> a >> b >> t;
+
+   int total_bis = (t/a) * b;
+
+
+    cout << total_bis << endl;
+    return 0;
+}
---
+++ XPSC/Week_One/Four_Digits.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    if (n < 10)
+    {
+        cout << "000" << n;
+    }
+    else if (n < 100)
+    {
+        cout << "00" << n;
+    }
+    else if (n < 1000)
+    {
+        cout << "0" << n;
+    }
+    else if (n < 10000)
+    {
+        cout  << n;
+    }
+
+    return 0;
+}
